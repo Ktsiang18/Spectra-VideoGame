4f9ca82ee0a8ee5ebe57badca592ac93fbdea4e7
Ktsiang18 <32723996+Ktsiang18@users.noreply.github.com>
2018-08-14 15:58:12 +1200

    Commenting for sample code changecolorcollision.cs

--- Assets/Scripts/ChangeColorCollision.cs
+++ Assets/Scripts/ChangeColorCollision.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//this script is placed on a bullet object
+//the bullets may be red, blue or yellow
 public class ChangeColorCollision : MonoBehaviour {
+	
+	//defines an array of colors the bullet may be changed to
 	public Material[] material;
 	Renderer rend;
 	public Collider2D colliderRed;
 	public Collider2D colliderYellow;
 	public Collider2D colliderBlue;
 
-
-
-	// Use this for initialization
-void Start () {
+	//the bullet will change color when shot through a collider
+	//initializes colliders
+	void Start () {
 		colliderRed = GetComponent<Collider2D> ();
 		colliderYellow = GetComponent<Collider2D> ();
 		colliderBlue = GetComponent<Collider2D> ();
@@ -20,19 +23,26 @@ void Start () {
 		rend.enabled = true;
 		rend.sharedMaterial = material [0];
 	}
+	
 	void OnTriggerEnter2D(Collider2D col)
 	{
+		//when the collider is blue, bullet changes to material[1]
+		//example: if bullet is red, its color would change to purple
 		if (col.gameObject.tag == "blue")
 		{
 			rend.sharedMaterial = material [1];
 			Debug.Log (rend.sharedMaterial);
 
 		}
+		//when the collider is yellow, bullet changes to material[2]
+		//example: if bullet is red, its color would change to orange
 		else if (col.gameObject.tag == "yellow")
 		{
 			rend.sharedMaterial = material [2];
 			Debug.Log ("yellow has been hit");
 		}
+		//when the collider is red, bullet changes to material[3]
+		//example: if bullet is red, its color would not change
 		else if  (col.gameObject.tag == "red")
 		{
 			rend.sharedMaterial = material [3];
@@ -40,6 +50,7 @@ void Start () {
 		}
 	}
 	// Update is called once per frame
+	//Checks if bullet is colliding with any of the three colliders
 	void Update(){
 		OnTriggerEnter2D (colliderRed);
 		OnTriggerEnter2D (colliderYellow);
